2596cb089cea9bd5322b6d8fd8a1400d8e4df4ed
javiergranadosr <javiergranadosrdev@gmail.com>
2022-07-31 18:39:22 -0500

    Se agrega maquetado de listado de vacantes

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,13 +5,17 @@ import { HomeRoutingModule } from './home-routing.module';
 import { BannerComponent } from './sections/banner/banner.component';
 import { HomeComponent } from './home/home.component';
 import { FiltersComponent } from './sections/filters/filters.component';
+import { ListVacantsComponent } from './sections/list-vacants/list-vacants.component';
+import { CardVacantComponent } from './components/card-vacant/card-vacant.component';
 
 
 @NgModule({
   declarations: [
     BannerComponent,
     HomeComponent,
-    FiltersComponent
+    FiltersComponent,
+    ListVacantsComponent,
+    CardVacantComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/home/sections/list-vacants/list-vacants.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-vacants',
+  templateUrl: './list-vacants.component.html',
+  styleUrls: ['./list-vacants.component.css']
+})
+export class ListVacantsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
