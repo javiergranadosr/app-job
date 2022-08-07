9426a9bae3eeaf94305cf9fa42ab0832f391fa4a
javiergranadosr <javiergranadosrdev@gmail.com>
2022-08-07 13:16:33 -0500

    Maquetado para publicar y crear vacante

--- src/app/vacants/new-vacant/new-vacant.component.ts
+++ src/app/vacants/new-vacant/new-vacant.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { DetailCategory } from 'src/app/home/sections/filters/interfaces/category';
+import { Salary } from 'src/app/home/sections/filters/interfaces/salary';
+import { FilterService } from 'src/app/home/sections/filters/services/filter.service';
 
 @Component({
   selector: 'app-new-vacant',
   templateUrl: './new-vacant.component.html',
-  styleUrls: ['./new-vacant.component.css']
+  styleUrls: ['./new-vacant.component.css'],
 })
 export class NewVacantComponent implements OnInit {
+  salaries: Salary[] = [];
+  categories: DetailCategory[] = [];
 
-  constructor() { }
+  constructor(private filterService: FilterService) {}
 
   ngOnInit(): void {
+    this.salaries = this.filterService.getSalaries();
+    this.filterService.getCategories().subscribe(({ categories, total }) => {
+      this.categories = categories;
+    });
   }
-
 }
