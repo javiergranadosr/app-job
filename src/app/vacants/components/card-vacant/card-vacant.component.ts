243dac8f6a354281e1d621dcad9bde17e8877015
javiergranadosr <javiergranadosrdev@gmail.com>
2022-08-07 16:03:02 -0500

    Maquetado listado de vacantes del reclutador

---
+++ src/app/vacants/components/card-vacant/card-vacant.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-vacant',
+  templateUrl: './card-vacant.component.html',
+  styleUrls: ['./card-vacant.component.css']
+})
+export class CardVacantComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/vacants/vacants.module.ts
+++ src/app/vacants/vacants.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { VacantsRoutingModule } from './vacants-routing.module';
 import { NewVacantComponent } from './new-vacant/new-vacant.component';
 import { VacantsComponent } from './vacants/vacants.component';
+import { CardVacantComponent } from './components/card-vacant/card-vacant.component';
 
 
 @NgModule({
   declarations: [
     NewVacantComponent,
-    VacantsComponent
+    VacantsComponent,
+    CardVacantComponent
   ],
   imports: [
     CommonModule,
